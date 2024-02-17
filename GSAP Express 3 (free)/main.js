20535a49b5e9c7b3d446f788ef1cd97f7b463575
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-17 21:22:38 +0200

    new changes

--- GSAP Express 3 (free)/main.js
+++ GSAP Express 3 (free)/main.js
@@ -1,2 +1,16 @@
 import "./style.css";
 import { gsap } from "gsap";
+
+gsap.set(".container", { scale: 0.7 });
+
+gsap
+  .timeline()
+  .from(".container", { opacity: 0, duration: 1 })
+  .from(".space-title", { opacity: 0, scale: 0, ease: "back" })
+  .from(".stars-container svg", {
+    y: 160,
+    stagger: 0.1,
+    duration: 0.6,
+    opacity: 0,
+    ease: "back",
+  });
