779d2c522a800a3964da6ad053ee1f6785ae94fb
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-18 20:58:23 +0200

    base changes

--- GSAP Express 3 (free)/main.js
+++ GSAP Express 3 (free)/main.js
@@ -1,2 +1,8 @@
 import "./style.css";
 import { gsap } from "gsap";
+
+const animation = gsap.timeline();
+animation
+  .to(".star", { duration: 2, x: 100 })
+  .to(".triangle", { duration: 3, x: 100 }, 3)
+  .to(".rectangle", { duration: 1, x: 100 }, "<0.5");
