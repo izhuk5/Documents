66ac4d72186c4e2f22a31fd03d0deb6bb3fdc564
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-20 18:18:55 +0200

    some changes

--- GSAP Express 3 (free)/main.js
+++ GSAP Express 3 (free)/main.js
@@ -7,10 +7,14 @@ gsap
   .timeline()
   .from(".container", { opacity: 0, duration: 1 })
   .from(".space-title", { opacity: 0, scale: 0, ease: "back" })
-  .from(".stars-container svg", {
-    y: 160,
-    stagger: 0.1,
-    duration: 0.6,
-    opacity: 0,
-    ease: "back",
-  });
+  .from(
+    ".stars-container svg",
+    {
+      y: 160,
+      stagger: 0.1,
+      duration: 0.6,
+      opacity: 0,
+      ease: "back",
+    },
+    "+=2"
+  );
