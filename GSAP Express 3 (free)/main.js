ea59147288442144f23a85d2cb976e8efe8f7dab
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-20 19:00:29 +0200

    base structure

--- GSAP Express 3 (free)/main.js
+++ GSAP Express 3 (free)/main.js
@@ -1,2 +1,13 @@
 import "./style.css";
 import { gsap } from "gsap";
+
+const item = document.querySelector(".item");
+gsap.defaults({ duration: 0.3 });
+
+const tl = gsap
+  .timeline({ paused: true })
+  .to(".text", { color: "purple", x: 10 })
+  .to(".dot", { backgroundColor: "orange", scale: 1.5 }, 0);
+
+item.addEventListener("mouseenter", () => tl.play());
+item.addEventListener("mouseleave", () => tl.reverse());
