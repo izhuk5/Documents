bd7887eacbe357fcc2c16c437a90079b8fa72860
Ihor Zhuk <ihorzhuk55555@icloud.com>
2024-02-21 18:31:45 +0200

    base

--- GSAP Express 3 (free)/main.js
+++ GSAP Express 3 (free)/main.js
@@ -1,13 +1,20 @@
 import "./style.css";
 import { gsap } from "gsap";
 
-const item = document.querySelector(".item");
+const items = document.querySelectorAll(".item");
 gsap.defaults({ duration: 0.3 });
 
-const tl = gsap
-  .timeline({ paused: true })
-  .to(".text", { color: "purple", x: 10 })
-  .to(".dot", { backgroundColor: "orange", scale: 1.5 }, 0);
+items.forEach(function (item, index) {
+  const tl = gsap
+    .timeline({ paused: true })
+    .to(item.querySelector(".text"), {
+      color: "purple",
+      x: 10,
+      scale: 1.3,
+      transformOrigin: "left center",
+    })
+    .to(item.querySelector(".dot"), { backgroundColor: "orange", scale: 1.5 }, 0);
 
-item.addEventListener("mouseenter", () => tl.play());
-item.addEventListener("mouseleave", () => tl.reverse());
+  item.addEventListener("mouseenter", () => tl.play());
+  item.addEventListener("mouseleave", () => tl.reverse());
+});
